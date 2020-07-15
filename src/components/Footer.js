052f5f528ca3bf45a8589cdad4656f9f1602e0f2
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-15 16:32:57 -0300

    foi

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Elaborado por:</h3>
+        <p>Jéssica Tavares, Henrique Baeta e Orlando Messias</p>
+      </div>
+    );
+  }
+}
+
+export default Footer;
