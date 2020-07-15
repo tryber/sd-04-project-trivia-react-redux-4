8768e976aee688f2e7436debb8c1b64e4c5a62c5
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-15 19:18:11 -0300

    tentando adiantar o Login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,29 +1,45 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import { Redirect } from 'react-router-dom';
 import fetchToken from '../actions';
 
 class Login extends Component {
   constructor(props) {
     super(props);
-    this.handleChange = this.handleChange.bind(this);
+    this.state = {
+      name: '',
+      email: '',
+      shouldRedirect: false,
+    }
+    this.handleClick = this.handleClick.bind(this);
   }
 
-  componentDidMount() {
+  handleClick() {
+    //chama a action que salva o login
+    //salva o name e o email no store e no local storage na chave palyer
+
     this.props.fetchToken();
+    this.setState({ shouldRedirect: true })
   }
 
   render() {
     console.log(this.props.token);
+    const { name, email, shouldRedirect } = this.state;
+    if (shouldRedirect) return <Redirect to="/game" />
     return (
       <div>
         <h1>Renderiza Aqui!</h1>
         <h2>Jessica Rules!</h2>
         <label htmlFor="input-name">User</label>
-        <input id="input-name" onChange={'a'} data-testid="input-player-name" type="text" />
+        <input id="input-name" onChange={(e) => this.setState({ name: e.target.value })}
+          data-testid="input-player-name" type="text" value={name}
+        />
         <label htmlFor="input-email">Email</label>
-        <input id="input-email" onChange={'a'} data-testid="input-gravatar-email" type="email" />
-        <button data-testid="btn-play">Login</button>
+        <input id="input-email" onChange={(e) => this.setState({ email: e.target.value })}
+          data-testid="input-gravatar-email" type="email" value={email}
+        />
+        <button data-testid="btn-play" onClick={this.handleClick}>Login</button>
       </div>
     );
   }
