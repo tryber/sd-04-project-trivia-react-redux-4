578f5bc5a4e5f2db11e11881fdc7e668f4fc25f4
henrique <hbaetaleite@gmail.com>
2020-07-15 17:35:43 -0300

    codeclimate v3

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,6 @@ import rootReducer from '../reducers';
 const composeEnhancers =
   (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default store;
