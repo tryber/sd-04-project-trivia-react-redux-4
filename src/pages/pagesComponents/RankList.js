e67cc1a584da9a94286de10c1e1ff79d43973b47
henrique <hbaetaleite@gmail.com>
2020-07-20 18:19:43 -0300

    adicionando data-testid

--- src/pages/pagesComponents/RankList.js
+++ src/pages/pagesComponents/RankList.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RankList = ({ player }) => (
+const RankList = ({ player, index }) => (
   <div>
     <img src={player.picture} alt="gravatar" />
-    <p>{player.name}</p>
-    <p>{player.score}</p>
+    <p data-testid={`player-name=${index}`}>{player.name}</p>
+    <p data-testid={`player-score-${index}`}>{player.score}</p>
   </div>
 );
 
 RankList.propTypes = {
   player: PropTypes.arrayOf(PropTypes.string).isRequired,
+  index: PropTypes.number.isRequired,
 };
 
 export default RankList;
