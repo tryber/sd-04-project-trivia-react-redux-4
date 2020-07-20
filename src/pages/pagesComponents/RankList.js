37fb8644019b806cd2446f2a8cffb087983e8054
henrique <hbaetaleite@gmail.com>
2020-07-20 15:42:04 -0300

    codeclimate v1

--- src/pages/pagesComponents/RankList.js
+++ src/pages/pagesComponents/RankList.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const RankList = ({ player }) => {
-  return (
-    <div>
-      <img src={player.picture} alt="gravatar" />
-      <p>{player.name}</p>
-      <p>{player.score}</p>
-    </div>
-  );
-};
+const RankList = ({ player }) => (
+  <div>
+    <img src={player.picture} alt="gravatar" />
+    <p>{player.name}</p>
+    <p>{player.score}</p>
+  </div>
+);
 
 RankList.propTypes = {
   player: PropTypes.arrayOf(PropTypes.string).isRequired,
